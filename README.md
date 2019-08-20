f45299090ef6ec8469e53a5254e068145ce1a879
Kyle Byungsu Min <kylemin@umich.edu>
2019-08-19 20:40:13 -0400

    first release

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # TASED-Net
-Temporally-Aggregating Spatial Encoder-Decoder for Video Saliency Detection (ICCV 2019)
+[TASED-Net: Temporally-Aggregating Spatial Encoder-Decoder for Video Saliency Detection (ICCV 2019)](https://arxiv.org/abs/1908.05786)
+
+## Overview
+TASED-Net is a novel fully-convolutional network architecture for video saliency detection. The main idea is simple but effective: spatially decoding 3D video features while jointly aggregating all the temporal information. TASED-Net significantly outperforms previous state-of-the-art approaches on all three major large-scale datasets of video saliency detection: DHF1K, Hollywood2, and UCFSports. We observe that our model is especially better at attending to salient moving objects.
+
+TASED-Net is currently leading the leaderboard of [DHF1K online benchmark](https://mmcheng.net/videosal/).
+
+## Video Saliency Detection
+Video saliency detection aims to model the gaze fixation patterns of humans when viewing a dynamic scene. Because the predicted saliency map can be used to prioritize the video information across space and time, this task has a number of applications such as video surveillance, video captioning, video compression, etc.
+
+## Examples
+We compare our TASED-Net to [ACLNet](https://arxiv.org/abs/1801.07424), which was the previously leading state-of-the-art method. As shown in the examples below, TASED-Net is better at attending to the salient information. We also would like to point out that TASED-Net has a much smaller network size (82 MB v.s. 252 MB).
+
+![](example/comparison1.gif)
+
+![](example/comparison2.gif)
+
+## Code Usage
+First, clone this repository and download [this weight
+file](https://drive.google.com/uc?export=download&id=11DLJkuhRHHdRziYc2dQBiyPzf6QGn041).
+Then, just run the code using
+
+`$ python main.py`
+
+This will generate frame-wise saliency maps.
+You can also specify the input and output directories as command-line arguments. For example,
+
+`$ python main.py ./example ./output`
+
+## Notes
+- We observed that there is a trade-off between AUC scores and the others. The released model is a modified version to increase AUC-J and s-AUC scores (NSS, CC, and SIM scores are slightly lower than the reported figures). This version achieves 0.898 AUC-J and 0.716 s-AUC on the test set of DHF1K (2.621 NSS, 0.466 CC, 0.336 SIM).
+
+- We recommend using PNG image files as input (although examples of this repository are in JPEG format).
+
+- For the encoder of TASED-Net, we use the [S3D network](https://arxiv.org/abs/1712.04851). We pretrained S3D on Kinetics-400 dataset using PyTorch and it achieves 72.08% top1 accuracy (top5: 90.35%) on the validation set of the dataset. We release [our weight file for S3D](https://github.com/kylemin/S3D.git) together this project. If you find it useful, you might want to consider citing our work.
+
+- For training, we recommend using [ViP](https://github.com/MichiganCOG/ViP.git), which is the video platform for general purposes in PyTorch.
+
+## Citation
+```
+@misc{min2019tasednet,
+    title={TASED-Net: Temporally-Aggregating Spatial Encoder-Decoder Network for Video Saliency Detection},
+    author={Kyle Min and Jason J. Corso},
+    year={2019},
+    eprint={1908.05786},
+    archivePrefix={arXiv},
+    primaryClass={cs.CV}
+}
+```
---
+++ main.py
@@ -0,0 +1,106 @@
+import sys
+import os
+import numpy as np
+import cv2
+import torch
+from model import TASED_v2
+from scipy.ndimage.filters import gaussian_filter
+
+def main():
+    ''' read frames in path_indata and generate frame-wise saliency maps in path_output '''
+    # optional two command-line arguments
+    path_indata = './example'
+    path_output = './output'
+    if len(sys.argv) > 1:
+        path_indata = sys.argv[1]
+        if len(sys.argv) > 2:
+            path_output = sys.argv[2]
+    if not os.path.isdir(path_output):
+        os.makedirs(path_output)
+
+    len_temporal = 32
+    file_weight = './TASED_v2.pt'
+
+    model = TASED_v2()
+
+    # load the weight file and copy the parameters
+    if os.path.isfile(file_weight):
+        print ('loading weight file')
+        weight_dict = torch.load(file_weight)
+        model_dict = model.state_dict()
+        for name, param in weight_dict.items():
+            if 'module' in name:
+                name = '.'.join(name.split('.')[1:])
+            if name in model_dict:
+                if param.size() == model_dict[name].size():
+                    model_dict[name].copy_(param)
+                else:
+                    print (' size? ' + name, param.size(), model_dict[name].size())
+            else:
+                print (' name? ' + name)
+
+        print (' loaded')
+    else:
+        print ('weight file?')
+
+    model = model.cuda()
+    torch.backends.cudnn.benchmark = False
+
+    # iterate over the path_indata directory
+    list_indata = [d for d in os.listdir(path_indata) if os.path.isdir(os.path.join(path_indata, d))]
+    list_indata.sort()
+    for dname in list_indata:
+        print ('processing ' + dname)
+        list_frames = [f for f in os.listdir(os.path.join(path_indata, dname)) if os.path.isfile(os.path.join(path_indata, dname, f))]
+        list_frames.sort()
+
+        # process in a sliding window fashion
+        if len(list_frames) >= 2*len_temporal-1:
+            path_outdata = os.path.join(path_output, dname)
+            if not os.path.isdir(path_outdata):
+                os.makedirs(path_outdata)
+
+            snippet = []
+            for i in range(len(list_frames)):
+                img = cv2.imread(os.path.join(path_indata, dname, list_frames[i]))
+                img = cv2.resize(img, (384, 224))
+                img = img[...,::-1]
+                snippet.append(img)
+
+                if i >= len_temporal-1:
+                    clip = transform(snippet)
+
+                    process(model, clip, path_outdata, i)
+
+                    # process first (len_temporal-1) frames
+                    if i < 2*len_temporal-2:
+                        process(model, torch.flip(clip, [1]), path_outdata, i-len_temporal+1)
+
+                    del snippet[0]
+
+        else:
+            print (' more frames are needed')
+
+
+def transform(snippet):
+    ''' stack & noralization '''
+    snippet = np.concatenate(snippet, axis=-1)
+    snippet = torch.from_numpy(snippet).permute(2, 0, 1).contiguous().float()
+    snippet = snippet.mul_(2.).sub_(255).div(255)
+
+    return snippet.view(1,-1,3,snippet.size(1),snippet.size(2)).permute(0,2,1,3,4)
+
+
+def process(model, clip, path_outdata, idx):
+    ''' process one clip and save the predicted saliency map '''
+    with torch.no_grad():
+        smap = model(clip.cuda()).cpu().data[0]
+
+    smap = (smap.numpy()*255.).astype(np.int)/255.
+    smap = gaussian_filter(smap, sigma=7)
+    cv2.imwrite(os.path.join(path_outdata, '%04d.png'%(idx+1)), (smap/np.max(smap)*255.).astype(np.uint8))
+
+
+if __name__ == '__main__':
+    main()
+
